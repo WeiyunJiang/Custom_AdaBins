07185686d7a35d60104b9fd8c08fc45195ccd7de
WeiyunJiang <22603074+WeiyunJiang@users.noreply.github.com>
2021-05-07 21:39:54 -0700

    train scr

--- args.py
+++ args.py
@@ -3,11 +3,9 @@ import argparse
 def depth_arg():
 
     parser = argparse.ArgumentParser('Train a model')
-    
-    parser.add_argument('--split',
-                        type=str,
-                        default='train'
-                        )
+    parser.add_argument('--steps_til_summary', default=25, type=int, help='steps to print loss')
+    parser.add_argument('--small_data_num', default=100, type=int, help='use small portion of dataset')
+    parser.add_argument('--seed', default=42, type=int, help='random seed')
     parser.add_argument('--epochs', default=25, type=int, help='number of total epochs to run')
     parser.add_argument('--n-bins', '--n_bins', default=80, type=int,
                         help='number of bins/buckets to divide depth range into')
@@ -18,7 +16,7 @@ def depth_arg():
     parser.add_argument('--final-div-factor', '--final_div_factor', default=100, type=float,
                         help="final div factor for lr")
 
-    parser.add_argument('--bs', default=16, type=int, help='batch size')
+    parser.add_argument('--batch_size', default=16, type=int, help='batch size')
     parser.add_argument('--validate-every', '--validate_every', default=100, type=int, help='validation period')
     parser.add_argument('--gpu', default=None, type=int, help='Which gpu to use')
     parser.add_argument("--name", default="UnetAdaptiveBins")
@@ -75,145 +73,3 @@ def depth_arg():
     parser.add_argument('--garg_crop', help='if set, crops according to Garg  ECCV16', action='store_true')
     args = parser.parse_args()
     return args
-def get_train_args():
-    """Get arguments needed in train.py."""
-    parser = argparse.ArgumentParser('Train a model')
-
-    add_common_args(parser)
-    add_train_test_args(parser)
-
-    parser.add_argument('--eval_steps',
-                        type=int,
-                        default=50000,
-                        help='Number of steps between successive evaluations.')
-    parser.add_argument('--lr',
-                        type=float,
-                        default=0.001,
-                        help='Learning rate.')
-    parser.add_argument('--l2_wd',
-                        type=float,
-                        default=0,
-                        help='L2 weight decay.')
-    parser.add_argument('--num_epochs',
-                        type=int,
-                        default=10,
-                        help='Number of epochs for which to train. Negative means forever.')
-    parser.add_argument('--drop_prob',
-                        type=float,
-                        default=0.2,
-                        help='Probability of zeroing an activation in dropout layers.')
-    '''
-    parser.add_argument('--metric_name',
-                        type=str,
-                        default='F1',
-                        choices=('NLL', 'EM', 'F1'),
-                        help='Name of dev metric to determine best checkpoint.')
-    '''
-    '''
-    parser.add_argument('--max_checkpoints',
-                        type=int,
-                        default=5,
-                        help='Maximum number of checkpoints to keep on disk.')
-    parser.add_argument('--max_grad_norm',
-                        type=float,
-                        default=5.0,
-                        help='Maximum gradient norm for gradient clipping.')
-    '''
-    parser.add_argument('--seed',
-                        type=int,
-                        default=224,
-                        help='Random seed for reproducibility.')
-
-    '''
-    parser.add_argument('--ema_decay',
-                        type=float,
-                        default=0.999,
-                        help='Decay rate for exponential moving average of parameters.')
-    '''
-
-    '''
-
-    
-
-    if args.metric_name == 'NLL':
-        # Best checkpoint is the one that minimizes negative log-likelihood
-        args.maximize_metric = False
-    elif args.metric_name in ('EM', 'F1'):
-        # Best checkpoint is the one that maximizes EM or F1
-        args.maximize_metric = True
-    else:
-        raise ValueError(f'Unrecognized metric name: "{args.metric_name}"')
-    '''
-    args = parser.parse_args()
-    return args
-
-
-def add_common_args(parser):
-    """Add arguments common to all 3 scripts: setup.py, train.py, test.py"""
-    parser.add_argument('--train_record_file',
-                        type=str,
-                        default='./data/train.npz')
-    parser.add_argument('--dev_record_file',
-                        type=str,
-                        default='./data/dev.npz')
-    parser.add_argument('--test_record_file',
-                        type=str,
-                        default='./data/test.npz')
-    parser.add_argument('--word_emb_file',
-                        type=str,
-                        default='./data/word_emb.json')
-    parser.add_argument('--char_emb_file',
-                        type=str,
-                        default='./data/char_emb.json')
-    parser.add_argument('--train_eval_file',
-                        type=str,
-                        default='./data/train_eval.json')
-    parser.add_argument('--dev_eval_file',
-                        type=str,
-                        default='./data/dev_eval.json')
-    parser.add_argument('--test_eval_file',
-                        type=str,
-                        default='./data/test_eval.json')
-    
-
-
-def add_train_test_args(parser):
-    """Add arguments common to train.py and test.py"""
-    parser.add_argument('--name',
-                        '-n',
-                        type=str,
-                        required=True,
-                        help='Name to identify training or test run.')
-    parser.add_argument('--max_ans_len',
-                        type=int,
-                        default=15,
-                        help='Maximum length of a predicted answer.')
-    parser.add_argument('--num_workers',
-                        type=int,
-                        default=4,
-                        help='Number of sub-processes to use per data loader.')
-    parser.add_argument('--save_dir',
-                        type=str,
-                        default='./save/',
-                        help='Base directory for saving information.')
-    parser.add_argument('--batch_size',
-                        type=int,
-                        default=64,
-                        help='Batch size per GPU. Scales automatically when \
-                              multiple GPUs are available.')
-    parser.add_argument('--use_squad_v2',
-                        type=lambda s: s.lower().startswith('t'),
-                        default=True,
-                        help='Whether to use SQuAD 2.0 (unanswerable) questions.')
-    parser.add_argument('--hidden_size',
-                        type=int,
-                        default=100,
-                        help='Number of features in encoder hidden layers.')
-    parser.add_argument('--num_visuals',
-                        type=int,
-                        default=10,
-                        help='Number of examples to visualize in TensorBoard.')
-    parser.add_argument('--load_path',
-                        type=str,
-                        default=None,
-                        help='Path to load as a model checkpoint.')
--- train.py
+++ train.py
@@ -1,65 +1,108 @@
-import argparse
-import os
-import sys
-import uuid
-from datetime import datetime as dt
-
 import numpy as np
 import torch
-import torch.distributed as dist
-import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data.distributed
-import wandb
+import random
+import time
+
+
 from tqdm import tqdm
 
-from models import VGG_16
+from models import VGG_16, UnetAdaptiveBins
 
 from dataio import Depth_Dataset
-from loss import SILogLoss
-from args import get_train_args, depth_arg
-from torch.utils.data import Dataset, DataLoader
+from loss import SILogLoss, BinsChamferLoss
+from args import depth_arg
+from torch.utils.data import DataLoader
+from torch.nn.utils import clip_grad_norm_
 
-'''
-def main(args):
 
-	
-	# choose device
-	gpu_ids = []
-    if torch.cuda.is_available():
-        gpu_ids += [gpu_id for gpu_id in range(torch.cuda.device_count())]
-        device = torch.device(f'cuda:{gpu_ids[0]}')
-        torch.cuda.set_device(device)
-    else:
-        device = torch.device('cpu')
-	
+def train_model(model, args, device=None):
+    # initialize dataset
+    train_dataset = Depth_Dataset(args.dataset, 'train', small_data_num = 100) 
+    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, 
+                                                               [int(0.9 * len(train_dataset)), 
+                                                                int(0.1 * len(train_dataset))])
+   	
+    train_data_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_data_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+    
+    # define loss criterion for depth and bin maps
+    criterion_depth = SILogLoss() 
+    criterion_bins = BinsChamferLoss()
+    
+    model.train(True)
+    
+    # we want to tune the parameter of the pretrained encoder more carefully
+    params = [{"params": model.get_1x_lr_params(), "lr": args.lr / 10},
+              {"params": model.get_10x_lr_params(), "lr": args.lr}]
+    
+    # define optimizer
+    optimizer = optim.AdamW(params, weight_decay=args.wd, lr=args.lr)
+    
+    # one cycle lr scheduler
+    scheduler = optim.lr_scheduler.OneCycleLR(optimizer, args.lr, epochs=args.epochs, 
+                                              steps_per_epoch=len(train_data_loader),
+                                              cycle_momentum=True,
+                                              base_momentum=0.85, max_momentum=0.95, 
+                                              last_epoch=args.last_epoch,
+                                              div_factor=args.div_factor,
+                                              final_div_factor=args.final_div_factor)
 
-	
+    total_steps = 0
+    
+    with tqdm(total=len(train_data_loader) * args.epochs) as pbar:
+        for epoch in range(args.epochs):
+            print("Epoch {}/{}".format(epoch, args.epochs))
+            print('-' * 10)
+ 
+            for step, batch in enumerate(train_data_loader):
+                start_time = time.time()
+                
+                # image(N, 3, 427, 565)
+                # depth(N, 1, 427, 565)
+                optimizer.zero_grad()
+                
+                image, depth = batch['image'], batch['depth']
+                image.to(device)
+                depth.to(device)
+                
+                bins, pred = model(image)
+
+                mask = depth > args.min_depth
+                mask = mask.to(torch.bool)
+                loss_depth = criterion_depth(pred, depth, mask=mask)
+                loss_bin = criterion_bins(bins, depth)
+                
+                loss = loss_depth + args.w_chamfer * loss_bin
+                loss.backward()
+                clip_grad_norm_(model.parameters(), 0.1)  # optional
+                optimizer.step()
+                
+                
+                scheduler.step()
+                
+                pbar.update(1)
+                
+                if not (total_steps+1) % args.steps_til_summary:
+                    tqdm.write("Epoch [%d/%d], Step [%d/%d], Loss: %.4f, iteration time %0.6f sec" 
+                    % (epoch, args.epochs, step, len(train_data_loader), loss, time.time() - start_time))
+                        
+                total_steps += 1
+                
+               
+
+
+if __name__ == '__main__': 
+    args = depth_arg()
     # Set random seed
-    log.info(f'Using random seed {args.seed}...')
+    print(f'Using random')
+    print(f'Using random seed {args.seed}')
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
-	
-
-	# build the dataset
-	dataset = Depth_Dataset(args.dataset, args.split)
-	if args.split == 'train':
-		train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(0.9 * len(train_dataset)), len(train_dataset)-int(0.9 * len(train_dataset))]) 
-
-    # build the model 
-    output_size = (320, 40)
-    model = VGG_16(output_size= output_size)
-    
-
-    # need optimizer
-    params = model.parameters
-    optimizer = optim.AdamW(params, weight_decay=args.wd, lr=args.lr)
-'''
-
-def train_model(model, lossF, optimizer, args, num_epochs=10):
     
     gpu_ids = []
     if torch.cuda.is_available():
@@ -68,92 +111,19 @@ def train_model(model, lossF, optimizer, args, num_epochs=10):
         torch.cuda.set_device(device)
     else:
         device = torch.device('cpu')
-    model.to(device)
-
-    
-    best_acc = 0.0
-    
-    avg_loss = 0
-    avg_acc = 0
-    avg_loss_val = 0
-    avg_acc_val = 0
-    
-    dataset = Depth_Dataset(args.dataset, args.split)
-    if args.split == 'train':
-    	train_dataset = Depth_Dataset('nyu', 'train', small_data_num = 100)
-    	train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(0.9 * len(train_dataset)), len(train_dataset)-int(0.9 * len(train_dataset))])
-    	train_data_loader = DataLoader(train_dataset, batch_size=90, shuffle=True)
-    	val_data_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-    
-    for epoch in range(num_epochs):
-        print("Epoch {}/{}".format(epoch, num_epochs))
-        print('-' * 10)
         
-        loss_train = 0
-        loss_val = 0
-        acc_train = 0
-        acc_val = 0
-        
-        model.train(True)
-        
-        lossF = lossF()
-        for i, data in enumerate(train_dataset):
-            '''
-            if i % 100 == 0:
-                print("\rTraining batch {}/{}".format(i, train_batches / 2), end='', flush=True)
-             
-           	'''
-
-            # inputs(3, 427, 565)
-            # labels(1, 427, 565)
-
-            inputs, labels = data['image'], data['depth']
-            inputs.to(device)
-            labels.to(device)
-
-            
-
-            a = inputs.size()
-            b = labels.size()
-            
-            # inputs(1, 3, 427, 565)
-            # labels(1, 1, 427, 565)
-            inputs = inputs.view(1, a[0], a[1], a[2])
-            labels = labels.view(1, b[0], b[1], b[2])
-            
-           
-            
-            
-            optimizer.zero_grad()
-            
-            
-            # pred 1,1,320,40
-            pred = model(inputs)
-            pred = pred.view(1, 1, 320, 240)
-            
-            
-            
-            mask = labels > args.min_depth
-            
-
-            
-            loss = lossF(pred, labels, mask=mask.to(torch.bool))
-            loss.backward()
-            print(loss)
-            
-            
-            optimizer.step()
-    return loss
-
-if __name__ == '__main__':
-	args = depth_arg()
-	output_size = (320, 240)
-	model = VGG_16(output_size= output_size)
-	params = model.parameters()
-
-	#optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-	optimizer = optim.AdamW(params, weight_decay=args.wd, lr=args.lr)
-	print(train_model(model, SILogLoss, optimizer, args, num_epochs=10))
+    model = UnetAdaptiveBins.build_encoder(n_bins=args.n_bins, min_val=args.min_depth, 
+                                           max_val=args.max_depth, norm=args.norm)
+    '''
+    output_size = (320, 240) 
+    model = VGG_16(output_size= output_size) 
+    params = model.parameters()
+    '''
+    model.to(device) 
+    args.epoch = 0 
+    args.last_epoch = -1
+    
+    train_model(model, args, device)
 
 
 
