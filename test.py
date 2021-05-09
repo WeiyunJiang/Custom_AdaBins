dab5d63ba8d03ba8670819fbc349af9799f10bfc
bchen72 <68482540+bchen72@users.noreply.github.com>
2021-05-09 00:17:03 -0700

    test_img

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import torch
 import torch.utils.data.distributed
 import random
 import os
-
 import utils
 
 from tqdm import tqdm
@@ -16,13 +15,16 @@ from args import depth_arg
 from torch.utils.data import DataLoader
 
 from evaluate import evaluate_model, RunningAverageDict
+import matplotlib.pyplot as plt
 
 
-def test(model, test_data_loader):
+
+def test(model, test_data_loader, args):
     with torch.no_grad():
         metrics_test = RunningAverageDict()
         model.eval()
-        
+        pred_list = []
+        depth_gt_list = []
         for step, batch in tqdm(enumerate(test_data_loader)):  
             # image(N, 3, 427, 565)
             # depth(N, 1, 427, 565)
@@ -32,14 +34,28 @@ def test(model, test_data_loader):
         
             bins, pred = model(image)
             evaluate_model(pred, depth, metrics_test, args)
-        
+            pred_list.append(pred[0])
+            depth_gt_list.append(depth[0])
         metrics_test_value = metrics_test.get_value()
         tqdm.write("SiLog Loss: %.4f, a1: %.4f, a2: %.4f, a3: %.4f, rel: %.4f, rms: %.4f, log10: %.4f" 
                 % (metrics_test_value['silog'], metrics_test_value['a1'], metrics_test_value['a2'],
                    metrics_test_value['a3'],metrics_test_value['abs_rel'],metrics_test_value['rmse'],
                    metrics_test_value['log_10']))
-
-
+        depth_gt = depth_gt_list[0] # (1, H, W)
+        
+        pred = pred_list[0]
+        depth_gt[depth_gt < args.min_depth] = args.min_depth
+        depth_gt[depth_gt > args.max_depth] = args.max_depth
+                    
+        colored_gt = utils.colorize(depth_gt, vmin=None, vmax=None, cmap='magma_r') # (H, W, 3)
+        colored_pred = utils.colorize(pred, vmin=None, vmax=None, cmap='magma_r') # (H, W, 3)
+        plt.imshow(colored_pred, cmap='magma_r')
+        plt.show()
+        plt.savefig(f'test_imgs/{args.exp_name}_colored_pred.jpg')
+        plt.figure()
+        plt.imshow(colored_gt, cmap='magma_r')
+        plt.show()
+        plt.savefig(f'test_imgs/{args.exp_name}_colored_gt.jpg')
 if __name__ == '__main__': 
     args = depth_arg()
     # Set random seed
@@ -71,7 +87,9 @@ if __name__ == '__main__':
                                            max_val=args.max_depth, norm=args.norm)
     model.load_state_dict(train_state_dict)
     model.to(device)
-    test(model, test_data_loader)
+    test(model, test_data_loader, args)
+    
+    
 
 
 
