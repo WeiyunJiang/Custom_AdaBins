3e244ab4d6ea5e642fbe2a27d9ab0d1aec8620dc
WY J <wyjiang@stanford.com>
2021-05-09 08:22:21 +0000

    demo

--- demo.py
+++ demo.py
@@ -8,22 +8,20 @@ from args import depth_arg
 import torch
 import numpy as np
 import utils
+import torch.nn as nn
 
 class InferenceHelper:
-    def __init__(self, args, device, dataset='nyu'):
+    def __init__(self, train_state_dict, args, device, dataset='nyu'):
         self.toTensor = ToTensor()
         self.device = device
         if dataset == 'nyu':
             self.min_depth = 1e-3
             self.max_depth = 10
             self.saving_factor = 1000  # used to save in 16 bit
-            model = UnetAdaptiveBins.build(n_bins=256, min_val=self.min_depth, max_val=self.max_depth)
-            
-        
-
-        model = model.load_state_dict(train_state_dict) 
-        model.eval()
-        self.model = model.to(self.device)
+            self.model = UnetAdaptiveBins.build_encoder(n_bins=80, min_val=self.min_depth, max_val=self.max_depth)
+        self.model.load_state_dict(train_state_dict) 
+        self.model.eval()
+        self.model.to(device)
 
     @torch.no_grad()
     def predict_pil(self, pil_image, visualized=False):
@@ -34,14 +32,41 @@ class InferenceHelper:
         bin_centers, pred = self.predict(img)
 
         if visualized:
+            print('visualized')
             viz = utils.colorize(torch.from_numpy(pred).unsqueeze(0), vmin=None, vmax=None, cmap='magma')
             # pred = np.asarray(pred*1000, dtype='uint16')
             viz = Image.fromarray(viz)
             return bin_centers, pred, viz
         return bin_centers, pred
+    
+    def predict(self, image):
+        bins, pred = self.model(image)
+        pred = np.clip(pred.cpu().numpy(), self.min_depth, self.max_depth)
 
+        # Flip
+        image = torch.Tensor(np.array(image.cpu().numpy())[..., ::-1].copy()).to(self.device)
+        pred_lr = self.model(image)[-1]
+        pred_lr = np.clip(pred_lr.cpu().numpy()[..., ::-1], self.min_depth, self.max_depth)
+
+        # Take average of original and mirror
+        final = 0.5 * (pred + pred_lr)
+        final = nn.functional.interpolate(torch.Tensor(final), image.shape[-2:],
+                                          mode='bilinear', align_corners=True).cpu().numpy()
+
+        final[final < self.min_depth] = self.min_depth
+        final[final > self.max_depth] = self.max_depth
+        final[np.isinf(final)] = self.max_depth
+        final[np.isnan(final)] = self.min_depth
+
+        centers = 0.5 * (bins[:, 1:] + bins[:, :-1])
+        centers = centers.cpu().squeeze().numpy()
+        centers = centers[centers > self.min_depth]
+        centers = centers[centers < self.max_depth]
+
+        return centers, final
 if __name__ == '__main__':
     args = depth_arg()
+    resolution = (320, 240)
     gpu_ids = []
     if torch.cuda.is_available():
         gpu_ids += [gpu_id for gpu_id in range(torch.cuda.device_count())]
@@ -52,12 +77,13 @@ if __name__ == '__main__':
     print(device)  
     
     img = Image.open("dataset/nyu_depth_v2/official_splits/test/classroom/rgb_00283.jpg")
+    img = img.resize((320, 240))
     start = time()
     root_path = os.path.join(args.logging_root, args.exp_name)
     checkpoints_dir = os.path.join(root_path, 'checkpoints')
     PATH = os.path.join(checkpoints_dir, 'model_best_val.pth')
     train_state_dict = torch.load(PATH)
-    inferHelper = InferenceHelper(args, device)
+    inferHelper = InferenceHelper(train_state_dict, args, device)
     centers, pred = inferHelper.predict_pil(img)
     print(f"took :{time() - start}s")
     plt.imshow(pred.squeeze(), cmap='magma_r')
